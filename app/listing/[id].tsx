e31738280da6a8bb19767a26b6b31600a8f488aa
NgothTsika <ngothtsikavivien@gmail.com>
2025-03-14 00:51:39 +0000

    done with DataPage

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -5,29 +5,117 @@ import {
   Image,
   Dimensions,
   TouchableOpacity,
+  Share,
+  StyleSheet,
 } from "react-native";
-import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import React, { useLayoutEffect } from "react";
+import { useLocalSearchParams, useNavigation } from "expo-router";
 import listingsData from "@/assets/data/airbnb-listings.json";
-import Animated from "react-native-reanimated";
+import Animated, {
+  interpolate,
+  useAnimatedRef,
+  useAnimatedStyle,
+  useScrollViewOffset,
+} from "react-native-reanimated";
 import { Listing } from "@/interfaces/listing";
 import { Ionicons } from "@expo/vector-icons";
 import { defaultStyles } from "@/constants/Styles";
 
 const { width } = Dimensions.get("window");
+const IMG_HEIGHT = 300;
 
-const Page = () => {
-  const { id } = useLocalSearchParams<{ id: string }>();
+const DataPage = () => {
+  const { id } = useLocalSearchParams();
   const listing: Listing = (listingsData as any[]).find(
     (item) => item.id === id
   );
+  const navigation = useNavigation();
+  const scrollRef = useAnimatedRef<Animated.ScrollView>();
+
+  const shareListing = async () => {
+    try {
+      await Share.share({
+        title: listing.name,
+        url: listing.listing_url,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: "",
+      headerTransparent: true,
+
+      headerBackground: () => (
+        <Animated.View
+          style={[
+            headerAnimatedStyle,
+            { backgroundColor: "#fff", height: 100, borderBottomWidth: 1 },
+          ]}
+        ></Animated.View>
+      ),
+      headerRight: () => (
+        <View className="flex-row items-center justify-center gap-2">
+          <TouchableOpacity
+            className="w-10 h-10 rounded-full bg-white items-center justify-center"
+            onPress={shareListing}
+          >
+            <Ionicons name="share-outline" size={22} color={"#000"} />
+          </TouchableOpacity>
+          <TouchableOpacity className="w-10 h-10 rounded-full bg-white items-center justify-center">
+            <Ionicons name="heart-outline" size={22} color={"#000"} />
+          </TouchableOpacity>
+        </View>
+      ),
+      headerLeft: () => (
+        <TouchableOpacity
+          className="w-10 h-10 rounded-full bg-white items-center justify-center"
+          onPress={() => navigation.goBack()}
+        >
+          <Ionicons name="chevron-back" size={24} color={"#000"} />
+        </TouchableOpacity>
+      ),
+    });
+  }, []);
+
+  const scrollOffset = useScrollViewOffset(scrollRef);
+  const imageAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: interpolate(
+            scrollOffset.value,
+            [-IMG_HEIGHT, 0, IMG_HEIGHT, IMG_HEIGHT],
+            [-IMG_HEIGHT / 2, 0, IMG_HEIGHT * 0.75]
+          ),
+        },
+        {
+          scale: interpolate(
+            scrollOffset.value,
+            [-IMG_HEIGHT, 0, IMG_HEIGHT],
+            [2, 1, 1]
+          ),
+        },
+      ],
+    };
+  });
+  const headerAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(scrollOffset.value, [0, IMG_HEIGHT / 1.5], [0, 1]),
+    };
+  }, []);
+
   return (
     <View className="flex-1 bg-white">
-      <ScrollView contentContainerStyle={{ paddingBottom: 100 }}>
-        <Image
+      <Animated.ScrollView
+        ref={scrollRef}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <Animated.Image
           source={{ uri: listing.xl_picture_url }}
-          className="h-[300px]"
-          width={width}
+          style={[imageAnimatedStyle, styles.image]}
           resizeMode="cover"
         />
         <View className="bg-white p-5">
@@ -65,13 +153,13 @@ const Page = () => {
 
           <View className="my-4 h-[1px] bg-gray-400" />
 
-          <Text className="text-base mt-2 font-medium ">
+          <Text className="text-base mt-2 font-medium tracking-wide ">
             {listing.description}
           </Text>
         </View>
-      </ScrollView>
+      </Animated.ScrollView>
       {/* footer */}
-      <View className="absolute bottom-0 left-0 right-0 bg-white p-5 border-t border-gray-200">
+      <Animated.View className="absolute bottom-0 left-0 right-0 bg-white p-5 border-t border-gray-200">
         <View className="flex flex-row justify-between items-center">
           <TouchableOpacity className="h-full justify-center flex-row items-center gap-1">
             <Text className="font-Montserrat-semibold text-lg">
@@ -83,9 +171,16 @@ const Page = () => {
             <Text style={defaultStyles.btnText}>Reserve</Text>
           </TouchableOpacity>
         </View>
-      </View>
+      </Animated.View>
     </View>
   );
 };
 
-export default Page;
+export default DataPage;
+
+const styles = StyleSheet.create({
+  image: {
+    height: IMG_HEIGHT,
+    width: width,
+  },
+});
